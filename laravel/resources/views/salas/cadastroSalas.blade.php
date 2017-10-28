c7c155b6b2048bd26d8010841deeaef9c9bec944
rafael2196 <rafael2196@gmail.com>
2017-10-28 01:07:34 -0200

    adicao de salas

---
+++ laravel/resources/views/salas/cadastroSalas.blade.php
@@ -0,0 +1,67 @@
+﻿<html>
+
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+
+<form action="listar">
+<div class="container">
+<div class="row justify-content-center">
+<div class="col-4">
+    <fieldset class="form-group" >
+    <legend class="align-middle">Cadastro de salas</legend>
+        <div class="col-4">
+            <label class="form-check-label">Selecione o campus: 
+                <select id="campus_select" value="0" >
+                    <option value="1">Asa Sul</option>
+                    <option value="2">Asa Norte</option>
+                    <option value="3">Oeste</option>
+                </select>
+            </label>
+        </div>
+        <div class="form-check">
+            <label class="form-check-label">Bloco: 
+                <select id="campus_select" value="0" >
+                    <option value="1">A</option>
+                    <option value="2">B</option>
+                    <option value="3">C</option>
+                    <option value="4">A</option>
+                    <option value="5">B</option>
+                    <option value="6">C</option>
+                </select>
+            </label>
+        </div>
+    </fieldset>
+
+
+
+
+
+    <fieldset class="form-group">
+        <div class="form-check">
+          <label class="form-check-label">Andar: 
+              <select id="campus_select" value="0" >
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+              </select>
+          </label>
+        </div>
+
+        <div class="form-group">
+            <label for="nomeDaSala">Nome da sala:</label>
+            <input type="text" class="glyphicon glyphicon-align-left" id="nomeDaSala" name="nomeDaSala" placeholder="AA1">
+        </div>
+        <div class="form-group">
+            <label for="Capacidade">Capacidade:</label>
+            <input class="glyphicon glyphicon-align-left" id="capacidade_sala" >
+        </div>
+    </fieldset>
+    <input type="button" class="btn btn-danger" value="Desistir" action="listar">
+    <input type="submit" class="btn btn-danger">
+</div>
+</div>
+</div>
+</form>
+
+</html>
---
+++ laravel/resources/views/salas/listarSalas.blade.php
@@ -0,0 +1,39 @@
+﻿<html>
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+
+<form>
+
+  <fieldset class="form-group" >
+    <legend class="align-middle">Pesquise uma sala:</legend>
+	<input type="text"\>
+	<br>
+	<input type="submit" class="btn btn-danger">
+	
+  </fieldset>
+
+</form>
+
+  <fieldset class="form-group" >
+    <legend class="align-middle"></legend>
+	<table class="table table-striped">
+	  <tr>
+	  <th>Nome da sala</th>
+	  <th>Campus</th>
+  	  <th>Descricão breve</th>
+	  </tr>
+	  <tr><td><a href='cadastro'>IA2</a></td><td>Asa Sul</td><td>Vire a direita no predio vermelho</td></tr>
+	  <tr><td><a href='cadastro'>BA1</a></td><td>Asa Norte</td><td>Vire a direita no predio Azul</td></tr>
+	  <tr><td><a href='cadastro'>CA1</a></td><td>Asa Sul</td><td>Ande reto ate nao der mais</td></tr>
+	  <tr><td><a href='cadastro'>BC2</a></td><td>Asa Sul</td><td>Vire a esquerda no predio vermelho</td></tr>
+	  
+	</table>
+	<ul class="pager">
+      <li><a href="#">Previous</a></li>
+      <li><a href="#">Next</a></li>
+    </ul>
+  </fieldset>
+
+
+</html>
