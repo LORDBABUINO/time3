d34c64ae80eb829d92a6f692170462e33494cf2d
LORDBABUINO <jorge.x7@gmail.com>
2017-10-29 16:29:25 -0200

    Primeira versão da view de cadastro de grade

--- laravel/resources/views/grade.blade.php
+++ laravel/resources/views/grade.blade.php
@@ -5,7 +5,7 @@
   <div class="row">
     <div class="col-md-8 col-md-offset-2">
       <div class="panel panel-default">
-        <div class="panel-heading">Dashboard</div>
+        <div class="panel-heading">Cadastro de Grade</div>
 
         <div class="panel-body">
           @if (session('status'))
@@ -14,9 +14,67 @@
             </div>
           @endif
 
+          {{Form::open()}}
 
-          <button>Salvar</button>
-          <button>Cancelar</button>
+          <div class="form-group">
+            {{Form::label('Semestre Atual:')}}
+            {{Form::text('semestre', null,
+                          array('required',
+                                'class' => 'form-control',
+                                'placeholder' => '3'
+                                ))}}
+          </div>
+          <div class="form-group">
+            {{Form::label('Curso:')}}
+            {{Form::select('curso', array('' => 'Selecione um Curso'), array('required',
+                  'class' => 'form-control'
+                  ))}}
+          </div>
+          <div class="form-group">
+            {{Form::label('disciplina:')}}
+            {{Form::select('disciplina', array('' => 'Selecione uma Disciplina'))}}
+          </div>
+          <div class="form-group">
+            {{Form::label('Dias de Aula:')}}
+            </br>
+            {{Form::label('Segunda:')}}
+            {{Form::checkbox('S', 'segunda')}}
+            {{Form::label('Terça:')}}
+            {{Form::checkbox('T', 'terca')}}
+            {{Form::label('Quarta:')}}
+            {{Form::checkbox('Q', 'quarta')}}
+            {{Form::label('Quinta:')}}
+            {{Form::checkbox('Q', 'quinta')}}
+            {{Form::label('Sexta:')}}
+            {{Form::checkbox('S', 'sexta')}}
+            {{Form::label('Sábado:')}}
+            {{Form::checkbox('S', 'sabado')}}
+          </div>
+          <div class="form-group">
+            {{Form::label('Turno:')}}
+            </br>
+            {{Form::label('Matutino:')}}
+            {{Form::radio('turno', 'matutino')}}
+            {{Form::label('Vespertino:')}}
+            {{Form::radio('turno', 'vespertino')}}
+            {{Form::label('Noturno:')}}
+            {{Form::radio('turno', 'noturno')}}
+          </div>
+          <div class="form-group">
+            {{Form::label('Campus:')}}
+            {{Form::select('campus', array('' => 'Selecione um Campus'))}}
+          </div>
+          <div class="form-group">
+            {{Form::label('Bloco:')}}
+            {{Form::select('bloco', array('' => 'Selecione um Bloco'))}}
+          </div>
+          <div class="form-group">
+            {{Form::label('Sala:')}}
+            {{Form::select('sala', array('' => 'Selecione uma Sala'))}}
+          </div>
+            <button>Salvar</button>
+            <button>Cancelar</button>
+          {{Form::close()}}
         </div>
       </div>
     </div>
